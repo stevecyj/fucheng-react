0a251854f6a453fe158a4b2d2062de8fc2a5d4d4
Steve <ycsteven.tsao@gmail.com>
2020-03-26 17:15:01 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TestComponent from './pages/TestComponent';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const str = 'hello';
+    const arr = [1, 2, 3];
+
+    console.log(str.includes('o'));
+    return (
+        <div className="App">
+            <TestComponent />
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/pages/TestComponent/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class index extends Component {
+    render() {
+        return <div>測試 es6</div>;
+    }
+}
+
+const count = 100;
+export { count };
+
+export default index;
