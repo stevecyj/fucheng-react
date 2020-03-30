89645ba3db982687ef1fc7a2955b3f7c96483fb6
Steve <ycsteven.tsao@gmail.com>
2020-03-30 17:03:20 +0800

    使用scss

--- src/pages/TestComponent/index.js
+++ src/pages/TestComponent/index.js
@@ -1,23 +1,16 @@
 import React, { Component } from 'react';
-import './style.css';
+import './style.scss';
 
 class index extends Component {
     render() {
         return (
-            <div>
+            <div className="wrap">
                 測試組件
-                <h1 style={{ color: 'red', fontSize: 36 }}>我是h1標題</h1>
+                <h1>我是h1標題</h1>
                 <p className="content">我是哈哈哈p段落</p>
-                <App user={{ id: '1' }} />
             </div>
         );
     }
 }
-
-const App = ({ user }) => {
-    let isLogin = false;
-    if (user && user.id) isLogin = true;
-    return <div>{isLogin ? <div>你已登錄</div> : <div>尚未登錄</div>}</div>;
-};
 
 export default index;
