595ad77d0fff547bef072b3cfdaabffc1a3f577f
Steve <ycsteven.tsao@gmail.com>
2020-03-31 15:06:34 +0800

    counter state

--- src/pages/Count/index.js
+++ src/pages/Count/index.js
@@ -2,11 +2,34 @@ import React, { Component } from 'react';
 import styles from './style.module.css';
 
 class index extends Component {
+    state = {
+        count: 0,
+        value: null
+    };
+
+    addCount() {
+        const { count } = this.state;
+        this.setState({ count: count + 1 });
+    }
+
     render() {
+        const { count } = this.state;
         return (
             <div className={styles.wrap}>
                 <header>我是頭部</header>
-                <section>我是內容</section>
+                <section>
+                    我是內容
+                    <br />
+                    <span>{count}</span>
+                    <button onClick={this.addCount.bind(this)}>增加</button>
+                    <br />
+                    <input
+                        type="text"
+                        value={this.state.value}
+                        onChange={e => this.setState({ value: e.target.value })}
+                    />
+                    <p>輸入的是：{this.state.value}</p>
+                </section>
                 <footer>我是底部</footer>
             </div>
         );
